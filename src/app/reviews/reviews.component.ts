601827a62b043730d5fff3147b6e2a3cbc312d54
aditya <adityasu12@gmail.com>
2021-01-31 16:41:57 +0530

    added student reviews

--- src/app/reviews/reviews.component.ts
+++ src/app/reviews/reviews.component.ts
@@ -9,6 +9,17 @@ export class ReviewsComponent implements OnInit {
 
   constructor() { }
 
+  paths=[
+    "/assets/reviews/r1.jpg",
+    "/assets/reviews/r2.jpg",
+    "/assets/reviews/r3.jpg",
+    "/assets/reviews/r4.jpg",
+    "/assets/reviews/r5.jpg",
+    "/assets/reviews/r6.jpg",
+    "/assets/reviews/r7.jpg",
+    "/assets/reviews/r8.jpg",
+  ]
+
   ngOnInit(): void {
   }
 
